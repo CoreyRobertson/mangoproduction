d662066dbcc8697b52c9af4bc224bd3357350c1a
coreyr <coreystephenr@gmail.com>
2023-05-15 11:17:50 +1000

    4th commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,26 +4,30 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
-const fadeIn = (direction, type, delay, duration) => ({
-    hidden: {
-        x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
-        y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
-        opacity: 0,
-    },
-    show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-            type,
-            delay,
-            duration,
-            ease: 'easeOut',
-        },
-    },
-});
+
 
 const Navbar = () => {
+
+    const fadeIn = (direction, type, delay, duration) => ({
+        hidden: {
+            x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+            y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+            opacity: 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+                type,
+                delay,
+                duration,
+                ease: 'easeOut',
+            },
+        },
+    });
+
+
     const [nav, setNav] = useState(false)
 
     const handleNav = () => {
@@ -38,19 +42,19 @@ const Navbar = () => {
             <div className={nav ? 'navbar_closed' : 'navbar_open'}>
                 <ul>
                     <Link href='/'>
-                        <motion.li variants={fadeIn('', 'tween', .1, .8)} className='nav_link' initial='hidden' whileInView='show' whileHover={{ scale: 1.1 }} onClick={() => handleNav(!nav)}>HOME</motion.li>
+                        <motion.li variants={fadeIn('up', 'tween', 0.2, 0.3)} className='nav_link' initial='hidden' whileInView='show' onClick={() => handleNav(!nav)}>HOME</motion.li>
                     </Link>
                     <Link href='/Packages'>
-                        <motion.li variants={fadeIn('', 'tween', .2, .8)} className='nav_link' initial='hidden' whileInView='show' whileHover={{ scale: 1.1 }} onClick={() => handleNav(!nav)}>PACKAGES</motion.li>
+                        <motion.li variants={fadeIn('up', 'tween', 0.4, 0.3)} className='nav_link' initial='hidden' whileInView='show' onClick={() => handleNav(!nav)}>PACKAGES</motion.li>
                     </Link>
                     <Link href='/Products'>
-                        <motion.li variants={fadeIn('', 'tween', .3, .8)} className='nav_link' initial='hidden' whileInView='show' whileHover={{ scale: 1.1 }} onClick={() => handleNav(!nav)}>SERVICES</motion.li>
+                        <motion.li variants={fadeIn('up', 'tween', 0.6, 0.3)} className='nav_link' initial='hidden' whileInView='show' onClick={() => handleNav(!nav)}>SERVICES</motion.li>
                     </Link>
                     <Link href='/Contact'>
-                        <motion.li variants={fadeIn('', 'tween', .4, .8)} className='nav_link' initial='hidden' whileInView='show' whileHover={{ scale: 1.1 }} onClick={() => handleNav(!nav)}>CONTACT</motion.li>
+                        <motion.li variants={fadeIn('up', 'tween', 0.8, 0.3)} className='nav_link' initial='hidden' whileInView='show' onClick={() => handleNav(!nav)}>CONTACT</motion.li>
                     </Link>
                     <Link href='/'>
-                        <motion.li variants={fadeIn('', 'tween', .5, .8)} className='nav_link' initial='hidden' whileInView='show' whileHover={{ scale: 1.1 }} onClick={() => handleNav(!nav)}>LOCATIONS</motion.li>
+                        <motion.li variants={fadeIn('up', 'tween', 1, 0.3)} className='nav_link' initial='hidden' whileInView='show' onClick={() => handleNav(!nav)}>LOCATIONS</motion.li>
                     </Link>
                 </ul>
             </div>
